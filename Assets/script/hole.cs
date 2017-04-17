fbdc38cfe7a60ee7b9a18bf3d86473beb52b37f1
adelazhong <294787895@qq.com>
2017-04-17 17:52:46 +0800

    Add files via upload

---
+++ Assets/script/hole.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hole : MonoBehaviour {
+	public GUIText loseText;
+	public AudioSource audio;
+	void OnTriggerEnter(Collider other)
+	{
+
+		if (other.gameObject.tag == "Player")
+		{
+			other.gameObject.SetActive(false);
+			audio.Play();
+			loseText.text="Game over!";
+		}
+	}
+}
---
+++ Assets/script/holes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class holes : MonoBehaviour {
+
+	public GUIText winText;
+	public AudioSource audio;
+	void OnTriggerEnter(Collider other)
+	{
+		
+		if (other.gameObject.tag == "Player")
+		{
+			other.gameObject.SetActive(false);
+			audio.Play();
+			winText.text="You win!";
+		}
+	}
+}
